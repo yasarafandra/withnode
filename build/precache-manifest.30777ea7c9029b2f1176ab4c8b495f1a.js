ede6b0102493e3cd419a2fb6a5c2d8e3ff8a756d
darmanto <darmanto@cititex.com>
2019-04-09 17:13:05 +0700

    new dengan node

---
+++ build/precache-manifest.30777ea7c9029b2f1176ab4c8b495f1a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "c45f7de008ab976a8e817e3c0e5095ca",
+    "url": "/static/media/Pe-icon-7-stroke.c45f7de0.svg"
+  },
+  {
+    "revision": "1b8d63b0419248c279ca",
+    "url": "/static/css/main.358ad8b3.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "524d720f3f670bd38785447ca9c4b395",
+    "url": "/static/media/BebasNeue-Bold.524d720f.otf"
+  },
+  {
+    "revision": "238fec4f4d9474314ab6",
+    "url": "/static/js/2.4accc75f.chunk.js"
+  },
+  {
+    "revision": "dcbd5e15c94fa6838d5ccd550016b9f3",
+    "url": "/static/media/logo.dcbd5e15.jpg"
+  },
+  {
+    "revision": "fc1c37129912de1227f33c7e370fa788",
+    "url": "/static/media/googleplus.fc1c3712.jpg"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "1b8d63b0419248c279ca",
+    "url": "/static/js/main.28eeeef1.chunk.js"
+  },
+  {
+    "revision": "db812d8a70a4e88e888744c1c9a27e89",
+    "url": "/static/media/fontawesome-webfont.db812d8a.woff2"
+  },
+  {
+    "revision": "32400f4e08932a94d8bfd2422702c446",
+    "url": "/static/media/fontawesome-webfont.32400f4e.eot"
+  },
+  {
+    "revision": "a35720c2fed2c7f043bc7e4ffb45e073",
+    "url": "/static/media/fontawesome-webfont.a35720c2.woff"
+  },
+  {
+    "revision": "a3de2170e4e9df77161ea5d3f31b2668",
+    "url": "/static/media/fontawesome-webfont.a3de2170.ttf"
+  },
+  {
+    "revision": "f775f9cca88e21d45bebe185b27c0e5b",
+    "url": "/static/media/fontawesome-webfont.f775f9cc.svg"
+  },
+  {
+    "revision": "f9d2ed7fa2540b452ff2feaad6eed886",
+    "url": "/static/media/size1.f9d2ed7f.svg"
+  },
+  {
+    "revision": "a105cda50ada8b1d3c5a401a5411f8ae",
+    "url": "/static/media/BebasNeue-Regular.a105cda5.otf"
+  },
+  {
+    "revision": "238fec4f4d9474314ab6",
+    "url": "/static/css/2.f30f0cf3.chunk.css"
+  },
+  {
+    "revision": "c14df5239a9a84b6c51a11401630336a",
+    "url": "/index.html"
+  }
+];
---
+++ build/static/js/index.js
@@ -0,0 +1,4 @@
+const output = 'Hello, World!'; // Define output
+
+// Send output to the console
+console.log(output)
